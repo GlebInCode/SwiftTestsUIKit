4a19498664cfba684ed5c26fb01406ce371aa507
Khamin Gleb <nimik19@yandex.ru>
2025-01-14 08:37:27 +0300

    feat (CG-02): Add API Kinopoisk

--- SwiftTestsUIKit/SceneDelegate.swift
+++ SwiftTestsUIKit/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let viewController = TestVC()
+        let viewController = MoviesView()
         window.rootViewController = viewController
         window.makeKeyAndVisible()
 
